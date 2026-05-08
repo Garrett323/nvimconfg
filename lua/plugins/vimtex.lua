920180777ef1e1fa740a528ea7b458972fcda048
Tim <tk>
2026-05-08 13:56:10 +0200

    switched to okular as viewer backend

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,7 +3,7 @@ return {
     "lervag/vimtex",
     lazy = false, -- load immediately for tex files
     init = function()
-      vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_view_method = "okular"
       vim.g.vimtex_quickfix_mode = 0
     end,
   },
